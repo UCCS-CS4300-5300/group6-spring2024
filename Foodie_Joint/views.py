69532ed30a1d43e0f58cf862bd4ee960585301b1
ChanzaZach <98930784+ChanzaZach@users.noreply.github.com>
2024-03-09 22:43:05 +0000

    Added form functionality. Fully added location adding

--- Foodie_Joint/views.py
+++ Foodie_Joint/views.py
@@ -1,20 +1,18 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 #from django.contrib import messages
-from django.http import HttpResponse
-from .models import Location, Item
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Location, Item, User, Review, ItemReview, Tag
+from .forms import LocationForm, ItemForm, UserForm, ReviewForm, ItemReviewForm, TagForm
 
 
 def index(request):
   # Since only one item can be recommended for now, we can just get the first item marked as recommended from the database.
   recommended_item = Item.objects.filter(is_recommended=True).first()
-  return render(request, 'templates/index.html',
-                {'recommended_item': recommended_item})
-
+  return render(request, 'templates/index.html', {'recommended_item': recommended_item})
 
 def base(request):
   return render(request, 'templates/base_template.html')
-
 
 def nearby(request):
   data = Location.objects.all()
@@ -23,3 +21,16 @@ def nearby(request):
 
 def login_user(request):
   return render(request, 'templates/login.html', {})
+
+def add_location(request):
+  submitted = False
+  if request.method == 'POST':
+    form = LocationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return HttpResponseRedirect('/add_location?submitted=True')
+  else:
+    form = LocationForm
+    if 'submitted' in request.GET:
+      submitted = True
+  return render(request, 'templates/add_location.html', {'form':form, 'submitted':submitted})
