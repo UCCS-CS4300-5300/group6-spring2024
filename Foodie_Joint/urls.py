5317144b35645486ccd9a47b1a8c45f5d16f7d73
ChanzaZach <98930784+ChanzaZach@users.noreply.github.com>
2024-03-10 03:26:37 +0000

    Added the ability to add items to the database. -ZC

--- Foodie_Joint/urls.py
+++ Foodie_Joint/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("base_template", views.base, name="base_template"),
     path("nearby", views.nearby, name="nearby"),
     path('login_user', views.login_user, name= 'login'),# pass zip through url? Or only work when user logged in?
-    path("add_location", views.add_location, name='add_location')
+    path("add_location", views.add_location, name='add_location'),
+    path("add_item", views.add_item, name='add_item')
 ]
