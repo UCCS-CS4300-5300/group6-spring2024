e2aa0530ac7ab31d4d710218cc334dbebfe0372d
ChanzaZach <98930784+ChanzaZach@users.noreply.github.com>
2024-03-10 18:49:05 +0000

    New changes ZC

--- test_chanza.py
+++ test_chanza.py
@@ -32,3 +32,4 @@ def test_urls():
   urlpatterns = Foodie_Joint.urls.urlpatterns
   assert 'add_location' in [url.name for url in urlpatterns]
   assert 'add_item' in [url.name for url in urlpatterns]
+
