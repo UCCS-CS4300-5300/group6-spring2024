41c6187c709890cd1b5295a9642823334119f0d0
lflancher <143742762+lflancher@users.noreply.github.com>
2024-03-11 01:17:50 +0000

    Pushing to my branch, the login, logout views and templates have been created/modified

--- Foodie_Joint/urls.py
+++ Foodie_Joint/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("base_template", views.base, name="base_template"),
     path("nearby", views.nearby, name="nearby"),
-    path('login_user', views.login_user, name= 'login'),# pass zip through url? Or only work when user logged in?
+    path('login_user', views.login_user, name= 'login'),
+    path('logout_user', views.logout_user, name = 'logout'),# pass zip through url? Or only work when user logged in?
 ]
--- Foodie_Joint/views.py
+++ Foodie_Joint/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
-#from django.contrib import messages
+from django.contrib import messages
 from django.http import HttpResponse
 from .models import Location, Item
 
@@ -22,4 +22,20 @@ def nearby(request):
   return render(request, 'templates/nearby.html', context)
 
 def login_user(request):
-  return render(request, 'templates/login.html', {})
+  if request.method == "POST":
+    username = request.POST.get("username")
+    password = request.POST.get("password")
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+      login(request, user)
+      return redirect("index")
+    else:
+      messages.error(request, "Invalid username or password")
+      return redirect("login")
+  else: 
+    return render(request, 'templates/login.html', {})
+
+def logout_user(request):
+  logout(request)
+  messages.success(request, ("Successfully logged out"))
+  return redirect("index")
