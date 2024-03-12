66e88ad14506bcec9f0d1f9deb7ecea1b246d65c
Derek Gary <jgary4151@gmail.com>
2024-03-12 17:21:25 +0000

    Update nearby view to include location.id

--- Foodie_Joint/urls.py
+++ Foodie_Joint/urls.py
@@ -5,10 +5,12 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("base_template", views.base, name="base_template"),
     path("nearby", views.nearby, name="nearby"),
-    path('login_user', views.login_user, name= 'login'),
-    path('logout_user', views.logout_user, name = 'logout'),
-    path('register_user', views.register_user, name = 'register'),
+    path('login_user', views.login_user, name='login'),
+    path('logout_user', views.logout_user, name='logout'),
+    path('register_user', views.register_user, name='register'),
     path("add_location", views.add_location, name='add_location'),
     path("add_item", views.add_item, name='add_item'),
-    path("<str:type>/<int:location_id>/location_details", views.show_location_items, name='location_details')
+    path("location/<int:location_id>/details",
+         views.show_location_items,
+         name='location_details'),
 ]
