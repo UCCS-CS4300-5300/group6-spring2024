6bb5f70f84bdc190598a1c0f772623bd15b350d5
Tyler Carroll <tcarroll@uccs.edu>
2024-04-16 05:01:03 +0000

    Added admin panel functionality for extended generic User model

--- Foodie_Joint/admin.py
+++ Foodie_Joint/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
-from .models import Location, Item, LocationTag, User, Review, ItemReview, Tag, ItemTag
+from .models import Location, Item, LocationTag, Account, Review, ItemReview, Tag, ItemTag
+
 
 # Create an Item Update Action and Interface for the Admin to Mark an Item as Recommended
 # Resources used: 'https://docs.djangoproject.com/en/5.0/ref/contrib/admin/actions/'
@@ -17,11 +20,22 @@ class ItemAdmin(admin.ModelAdmin):
                    "description"]  # Fields to allow for editing in Admin panel
   ordering = ["name"]  # Order by name in ascending order
 
+# Extended the generic Django User model to include some account details (address, etc.).
+# This makes it so that this relationship can be seen/edited in the Admin panel.
+# Resource: https://www.youtube.com/watch?v=kRJpQxi2jIo&ab_channel=JimShapedCoding
+class AccountInline(admin.StackedInline):
+  model = Account
+  can_delete = False
+  verbose_name_plural = 'Accounts'
+
+class CustomUserAdmin(UserAdmin):
+  inlines = (AccountInline, )
 
 admin.site.register(Location)
 admin.site.register(LocationTag)
 admin.site.register(Item, ItemAdmin)
-admin.site.register(User)
+admin.site.unregister(User)  # Unregistering the default Django User model
+admin.site.register(User, CustomUserAdmin)  # Registering the Custom User model
 admin.site.register(Review)
 admin.site.register(ItemReview)
 admin.site.register(Tag)
