838e6707804df2b4739c5a3f20361e20b7d2279c
ChanzaZach <98930784+ChanzaZach@users.noreply.github.com>
2024-04-29 22:17:49 +0000

    test changes

--- test_chanza.py
+++ test_chanza.py
@@ -23,7 +23,6 @@ def test_add_item_form():
   form = ItemForm()
   assert form.fields['name'].required
   assert form.fields['description'].required
-  assert form.fields['location'].required
   assert form.fields['is_recommended'].required is False
   assert form.fields['image'].required is False
 
@@ -46,13 +45,13 @@ def test_add_location_failure():
 @pytest.mark.django_db
 def test_add_item():
   image = SimpleUploadedFile(name='test_image.png', content=open('media/images/foodie-joint-logo.png', 'rb').read(), content_type='image/png')
-  itemForm = ItemForm(data={'name': 'test', 'description': 'test', 'location': Location.objects.first(), 'is_recommended': False, 'image': image})
+  itemForm = ItemForm(data={'name': 'test', 'description': 'test', 'is_recommended': False, 'image': image})
   assert itemForm.is_valid()
 
 @pytest.mark.django_db
 def test_add_item_failure():
   image = SimpleUploadedFile(name='test_image.png', content=open('media/images/foodie-joint-logo.png', 'rb').read(), content_type='image/png')
-  itemForm = ItemForm(data={'name': '51aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaas', 'description': 'test', 'location': Location.objects.first(), 'is_recommended': False, 'image': image})
+  itemForm = ItemForm(data={'name': '51aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaas', 'description': 'test', 'is_recommended': False, 'image': image})
   assert not itemForm.is_valid()
 
 def test_urls():
