8274bac4207f9dbae01fd29f1b4f7b735e4533c8
ChanzaZach <98930784+ChanzaZach@users.noreply.github.com>
2024-04-30 22:23:35 +0000

    Test updates

--- test_chanza.py
+++ test_chanza.py
@@ -3,9 +3,11 @@ import django
 django.setup()
 import Foodie_Joint.urls
 import Foodie_Joint.views
-from Foodie_Joint.models import Location, TagItem
+from Foodie_Joint.models import Location
 from Foodie_Joint.forms import LocationForm, ItemForm
 from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import TestCase, Client
+from django.urls import reverse
 
 # Run these tests with 'pytest'
 
@@ -28,8 +30,6 @@ def test_add_item_form():
 @pytest.mark.django_db
 def test_add_location():
   # Choosing a random tag to be selected for the location - uses tag.pk
-  
-  
   image = SimpleUploadedFile(name='test_image.png', content=open('media/images/foodie-joint-logo.png', 'rb').read(), content_type='image/png')
   locationForm = LocationForm(data={'name': 'test', 'description': 'test', 'location_type': next(iter(Location.LOCATION_CHOICES.keys())), 'address': 'test', 'tags': [1], 'image': image})
   assert locationForm.is_valid()
@@ -56,4 +56,15 @@ def test_urls():
   urlpatterns = Foodie_Joint.urls.urlpatterns
   assert 'add_location' in [url.name for url in urlpatterns]
   assert 'add_item' in [url.name for url in urlpatterns]
+
+class TestViews(TestCase):
+  @pytest.mark.django_db
+  def test_add_location(self):
+    client = Client()
+    response = client.post(reverse('add_location'), {'name': 'test', 'description': 'test', 'location_type': next(iter(Location.LOCATION_CHOICES.keys())), 'address': 'test', 'tags': [1]})
+    assert response.status_code == 302
   
+  def test_favorite_item(self):
+    client = Client()
+    response = client.post(reverse('favorite_item', args=[1]))
+    assert response.status_code == 302
